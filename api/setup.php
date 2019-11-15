d08599895af524115692b87dc2815c74240828e8
scshasha <shasha.christopher@gmail.com>
2019-11-15 15:25:58 +0200

    Comleted the database setup file

--- api/setup.php
+++ api/setup.php
@@ -2,19 +2,31 @@
 // Database setup script
 
 require_once('./connect.php');
-// select db to use
-$str = "USE `".DB_NAME."`;\n\n";
 
-// clear the existing table
-$str = "DROP TABLE `rd_users`;\n\n";
+try {
+	// select db to use
+	$str = "USE `".DB_NAME."`;\n\n";
 
-// recreate the table
-$str = "CREATE TABLE `rd_users`;\n\n";
+	// clear the existing table
+	$str = "DROP TABLE `rd_users`;\n\n";
 
+	// recreate the table
+	$str = "CREATE TABLE `rd_users` (\n".
+	"`id` int(11) AUTO_INCREMENT NOT NULL,\n".
+	"`first_name` varchar(45) NOT NULL,\n".
+	"`last_name` varchar(45) NOT NULL,\n".
+	"`email` varchar(45) NOT NULL,\n".
+	"`phone` varchar(45) NOT NULL\n".
+	"PRIMARY KEY(`id`)\n".
+	");";
 
-// prepare the sql stmnt
+	$query = $str;
+	// exec the query
+	$con->exec($query);
+	echo json_encode("Success!");
+} catch (PDOException $e) {
+	die($e->getMessage());
+}
 
-
-// exec the query
 
 

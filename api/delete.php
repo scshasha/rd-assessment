92f6ca49fdac3bf081d8b778c2744f0c4921881c
scshasha <shasha.christopher@gmail.com>
2019-11-16 15:22:44 +0200

    added delete end point

---
+++ api/delete.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * deletes a specified user
+ */
+
+require_once('./connect.php');
+
+$id = ($_GET['id'] !== null && (int)$_GET['id'] > 0) ? mysqli_real_escape_string($con, (int)$_GET['id']) : false;
+
+if (!$id) {
+	return http_response_code(400);
+}
+
+$sql = "DELETE FROM `rd_users` WHERE `id` = '{$id}' LIMIT 1";
+if (mysqli_query($con, $sql)) {
+	return http_response_code(204);
+} else {
+	return http_response_code(422);
+}
